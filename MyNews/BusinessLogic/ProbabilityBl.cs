b1cd7cdf2a77d08449f75d3b683259759603541c
MartinSob <martin.sobrero.m@gmail.com>
2021-11-26 09:26:16 -0300

    Fix when no positive tags

--- MyNews/BusinessLogic/ProbabilityBl.cs
+++ MyNews/BusinessLogic/ProbabilityBl.cs
@@ -16,90 +16,97 @@ namespace BusinessLogic
 		/// <param name="post"></param>
 		/// <returns> Probability </returns>
 		public Probability calculate(Post post) {
-			post.paragraphs = Regex.Matches(post.body, "[^\r\n]+((\r|\n|\r\n)[^\r\n]+)*").Count;
-			post.words = new PostBl().countWords(post.body);
-
 			Probability probability = new Probability();
 
-			double recommendedWords = 0;
-			double recommendedParagraphs = 0;
-			double recommendedTitle = 0;
+			try {
+				post.paragraphs = Regex.Matches(post.body, "[^\r\n]+((\r|\n|\r\n)[^\r\n]+)*").Count;
+				post.words = new PostBl().countWords(post.body);
 
-			foreach (Tag tag in post.tags) {
-				probability.probOpen += dao.getOpen(tag);
-				probability.probRead += dao.getRead(tag);
+				double recommendedWords = 0;
+				double recommendedParagraphs = 0;
+				double recommendedTitle = 0;
 
-				var postivePosts = dao.getPositivePosts(tag);
-				recommendedWords += calculateAvgWords(postivePosts);
-				recommendedParagraphs += calculateAvgParagrphs(postivePosts);
-				recommendedTitle += calculateAvgTitle(postivePosts);
-			}
+				foreach (Tag tag in post.tags) {
+					probability.probOpen += dao.getOpen(tag);
+					probability.probRead += dao.getRead(tag);
 
-			// Get the total of open and read tags
-			double totalOpen = dao.getTotalOpen();
-			double totalRead = dao.getTotalRead();
-
-			// Average of words, paragraphs and title length in the most liked posts of each of the tags
-			recommendedWords = recommendedWords / post.tags.Count;
-			recommendedParagraphs = recommendedParagraphs / post.tags.Count;
-			recommendedTitle = recommendedTitle / post.tags.Count;
-
-			// How many of the total opened/read have the selected tags
-			probability.probOpen = probability.probOpen / totalOpen;
-			probability.probRead = probability.probRead / totalRead;
-
-			// Difference between the length the of the post and the average of words, paragraphs and title in the most liked posts of each of the tags
-			probability.probParagraph = post.paragraphs > recommendedParagraphs ? recommendedParagraphs / post.paragraphs : post.paragraphs / recommendedParagraphs;
-			probability.probWords = post.words > recommendedWords ? recommendedWords / post.words : post.words / recommendedWords;
-			probability.probTitle = post.title.Length > recommendedTitle ? recommendedTitle / post.title.Length : post.title.Length / recommendedTitle;
-
-			// Add recommendations based on each probability
-			if (probability.probOpen * 100 < 60)
-				probability.recommendations.Add("error.tag_low_open_probability");
-
-			if (probability.probOpen * 100 > 80)
-				probability.recommendations.Add("success.tag_high_open_probability");
-
-			if (probability.probRead * 100 < 60)
-				probability.recommendations.Add("error.tag_low_read_probability");
-
-			if (probability.probRead * 100 > 80)
-				probability.recommendations.Add("success.tag_high_read_probability");
-
-			if (probability.probParagraph * 100 < 60) {
-				if (post.paragraphs > recommendedParagraphs) {
-					probability.recommendations.Add("error.too_many_paragraphs");
-				} else {
-					probability.recommendations.Add("error.too_few_paragraphs");
+					var postivePosts = dao.getPositivePosts(tag);
+					recommendedWords += calculateAvgWords(postivePosts);
+					recommendedParagraphs += calculateAvgParagrphs(postivePosts);
+					recommendedTitle += calculateAvgTitle(postivePosts);
 				}
-			}
 
-			if (probability.probParagraph * 100 > 80)
-				probability.recommendations.Add("success.good_count_paragraphs");
+				// Get the total of open and read tags
+				double totalOpen = dao.getTotalOpen();
+				double totalRead = dao.getTotalRead();
 
-			if (probability.probWords * 100 < 60) {
-				if (post.words > recommendedWords) {
-					probability.recommendations.Add("error.too_many_words");
-				} else {
-					probability.recommendations.Add("error.too_few_words");
+				// Average of words, paragraphs and title length in the most liked posts of each of the tags
+				recommendedWords = recommendedWords / post.tags.Count;
+				recommendedParagraphs = recommendedParagraphs / post.tags.Count;
+				recommendedTitle = recommendedTitle / post.tags.Count;
+
+				// How many of the total opened/read have the selected tags
+				probability.probOpen = probability.probOpen / totalOpen;
+				probability.probRead = probability.probRead / totalRead;
+
+				// Difference between the length the of the post and the average of words, paragraphs and title in the most liked posts of each of the tags
+				probability.probParagraph = post.paragraphs > recommendedParagraphs ? recommendedParagraphs / post.paragraphs : post.paragraphs / recommendedParagraphs;
+				probability.probWords = post.words > recommendedWords ? recommendedWords / post.words : post.words / recommendedWords;
+				probability.probTitle = post.title.Length > recommendedTitle ? recommendedTitle / post.title.Length : post.title.Length / recommendedTitle;
+
+				// Add recommendations based on each probability
+				if (probability.probOpen * 100 < 60)
+					probability.recommendations.Add("error.tag_low_open_probability");
+
+				if (probability.probOpen * 100 > 80)
+					probability.recommendations.Add("success.tag_high_open_probability");
+
+				if (probability.probRead * 100 < 60)
+					probability.recommendations.Add("error.tag_low_read_probability");
+
+				if (probability.probRead * 100 > 80)
+					probability.recommendations.Add("success.tag_high_read_probability");
+
+				if (probability.probParagraph * 100 < 60) {
+					if (post.paragraphs > recommendedParagraphs) {
+						probability.recommendations.Add("error.too_many_paragraphs");
+					} else {
+						probability.recommendations.Add("error.too_few_paragraphs");
+					}
 				}
-			}
 
-			if (probability.probWords * 100 > 80)
-				probability.recommendations.Add("success.good_count_words");
+				if (probability.probParagraph * 100 > 80)
+					probability.recommendations.Add("success.good_count_paragraphs");
 
-			if (probability.probTitle * 100 < 60) {
-				if (post.title.Length > recommendedTitle) {
-					probability.recommendations.Add("error.too_long_title");
-				} else {
-					probability.recommendations.Add("error.too_short_title");
+				if (probability.probWords * 100 < 60) {
+					if (post.words > recommendedWords) {
+						probability.recommendations.Add("error.too_many_words");
+					} else {
+						probability.recommendations.Add("error.too_few_words");
+					}
 				}
+
+				if (probability.probWords * 100 > 80)
+					probability.recommendations.Add("success.good_count_words");
+
+				if (probability.probTitle * 100 < 60) {
+					if (post.title.Length > recommendedTitle) {
+						probability.recommendations.Add("error.too_long_title");
+					} else {
+						probability.recommendations.Add("error.too_short_title");
+					}
+				}
+
+				if (probability.probTitle * 100 > 80)
+					probability.recommendations.Add("success.good_title_length");
+
+				return calculateValue(probability);
+
+			} catch (Exception e) {
+				Console.WriteLine(e);
 			}
 
-			if (probability.probTitle * 100 > 80)
-				probability.recommendations.Add("success.good_title_length");
-
-			return calculateValue(probability);
+			return probability;
 		}
 
 		double calculateAvgWords(List<Post> posts) {
